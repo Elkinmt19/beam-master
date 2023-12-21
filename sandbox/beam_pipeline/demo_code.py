e71b45ed6d03ef2b6b7cd01f6f0d87b8fce14778
Elkin Javier Guerra Galeano <elkinmt19@gmail.com>
2023-12-21 17:49:58 -0500

    Added the first example using Beam

---
+++ sandbox/beam_pipeline/demo_code.py
@@ -0,0 +1,15 @@
+import apache_beam as beam
+
+p1 = beam.Pipeline()
+
+attendance_count = (
+    p1
+    |beam.io.ReadFromText('data/dept_data.txt')
+    | beam.Map(lambda record: record.split(','))
+    |beam.Filter(lambda record: record[3] == 'Accounts')
+    |beam.Map(lambda record: (record[1], 1))
+    |beam.CombinePerKey(sum)
+    |beam.io.WriteToText('data/output_new_final')
+)
+
+p1.run()
